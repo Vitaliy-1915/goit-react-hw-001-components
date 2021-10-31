313916cac6ebf9d271ff08690caccf0743330480
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-10-31 22:41:07 +0200

    Добавил стили для Profile

--- src/components/WrapperProfile/WrapperProfile.jsx
+++ src/components/WrapperProfile/WrapperProfile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Profile from '../Profile/Profile';
+import s from '../WrapperProfile/WrapperProfile.module.css';
 
 function WrapperProfile(props) {
   const { user } = props;
 
   return (
-    <div>
+    <div className={s.container}>
       <Profile
         name={user.name}
         tag={user.tag}
